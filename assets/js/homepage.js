8c4c6905a014890c841c5310d31578ebcf8ea5dc
Owen Taylor <Owentaylor60@yahoo.com>
2022-08-04 20:36:49 -0400

    test

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,19 @@
 var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+
+    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+      response.json().then(function(data) {
+
+
+      })
+  });
+
 };
 getUserRepos();
+var x = 0;
+
+var f = document.createElement('h1');
+
+
+f.textContent = "1"
+
+f.textContent = "2"
